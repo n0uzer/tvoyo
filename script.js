72a0bd8a2b3a7f6a83baeb217b7694d0fbd9384f
n0uzer <sabfir0@gmail.com>
2025-11-29 23:16:23 +0500

    доделай banner

---
+++ script.js
@@ -0,0 +1,55 @@
+window.onscroll = function () {
+    const element = document.querySelector(".submenu");
+    const header = document.querySelector("header");
+
+    if (window.pageYOffset > 300) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+
+    if (window.pageYOffset > 200) {
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+};
+function updateTimer() {
+    const now = new Date();
+    // console.log(now);
+
+    const currentYear = now.getFullYear();
+    // console.log(currentYear);
+    const newYear = new Date(`January 1, 2026 00:00:00`);
+
+    const diff = newYear - now;
+    
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.querySelector(".days").textContent = days;
+    document.querySelector(".hours").textContent =
+        hours < 10 ? "0" + hours : hours;
+    document.querySelector(".minutes").textContent =
+        minutes < 10 ? "0" + minutes : minutes;
+    document.querySelector(".seconds").textContent =
+        seconds < 10 ? "0" + seconds : seconds;
+}
+
+updateTimer();
+setInterval(updateTimer, 1000);
+document.body.addEventListener('click', (e) => {
+    if(e.target.classList.contains('bi-heart') && e.target.parentElement.classList.contains('price')){
+        e.target.className = 'bi bi-heart-fill';
+        e.target.style.color = 'red';
+        return
+    }
+    
+    if(e.target.classList.contains('bi-heart-fill') && e.target.parentElement.classList.contains('price')){
+        e.target.className = 'bi bi-heart';
+    }
+});
+
